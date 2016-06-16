e2085e0977d4b87a0aaf4debf4ff0c0cbceaca88
Jonathan Pahl <jdpahl122@gmail.com>
2016-06-15 20:27:22 -0400

    Add initial views and styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: 'static_pages#home'
+  root 'static_pages#home'
+  get  'help' => 'static_pages#help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
